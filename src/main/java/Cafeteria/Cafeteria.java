db3db9a3a9aefef06cd4e6b16499a0bf972abd90
Maye <veyron92i@MacBook-Pro-de-Maye.local>
2025-11-22 14:37:03 +0100

    Primer commit: estructura base del proyecto

---
+++ src/main/java/Cafeteria/Cafeteria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Cafeteria;
+
+/**
+ *
+ * @author veyron92i
+ */
+public class Cafeteria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
